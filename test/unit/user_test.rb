a2b98ba12322746cb0477f15f9f996ad8a610bde
Shaun F.D Jackson <shaun.jackson@gryphontranslations.com>
2013-09-19 00:30:52 +0200

    User profile name error fixed for reg

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -28,12 +28,21 @@ class UserTest < ActiveSupport::TestCase
   end
 
   test "profile name formatting no spaces" do
-     user = User.new
+     user = User.new(first_name: 'Shaun', last_name: "Jackson", email: "shaun@shaun1.com")
+     user.password = user.password_confirmation = 'afafafafaafafa'
+     
      user.profile_name = "My profile without spaces"
 
-      assert !user.save
+     assert !user.save
      assert !user.errors[:profile_name].empty?
      assert !user.errors[:profile_name].include?("Must be formatted correctly.")
+  end
+
+  test "a user must have a correctly formatted profile name" do
+    user = User.new(first_name: 'Shaun', last_name: "Jackson", email: "shaun@shaun1.com")
+    user.password = user.password_confirmation = 'afafafafaafafa'
+    user.profile_name = 'shaun'
+    assert user.valid?
   end
 end
 
